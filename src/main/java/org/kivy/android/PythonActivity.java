09bf1223b30eceefd3b3c729d1997df47e2a28be
Tim Speelman <timspeelman@live.nl>
2020-07-02 20:54:57 +0200

    Add ipv8app oncreate to pyactivity

--- src/main/java/org/kivy/android/PythonActivity.java
+++ src/main/java/org/kivy/android/PythonActivity.java
@@ -51,7 +51,35 @@ import org.kivy.android.PythonUtil;
 import org.renpy.android.ResourceManager;
 import org.renpy.android.AssetExtract;
 
+// <IPV8APP>
+import org.openwallet.android.R;
+import android.Manifest;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatDelegate;
+import android.text.TextUtils;
+import android.webkit.WebSettings;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebResourceError;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import java.lang.Runnable;
+import org.openwallet.android.ServiceOpenwallet;
+// </IPV8APP>
+
 public class PythonActivity extends Activity {
+
+    // <IPV8MAIN>
+    private Class<?> mClss;
+    private WebView mWebView;
+    private JavaScriptInterface JSInterface;
+    private static final int ZBAR_CAMERA_PERMISSION = 1;
+    private static final int WRITE_STORAGE_PERMISSION_REQUEST_CODE = 110;
+    private static final String url = "http://127.0.0.1:8642/app/index.html";
+    // </IPV8MAIN>
+
     // This activity is modified from a mixture of the SDLActivity and
     // PythonActivity in the SDL2 bootstrap, but removing all the SDL2
     // specifics.
@@ -68,6 +96,60 @@ public class PythonActivity extends Activity {
     private ResourceManager resourceManager = null;
     private Bundle mMetaData = null;
     private PowerManager.WakeLock mWakeLock = null;
+
+    // <IPV8APP>
+
+    protected void ipv8AppOnCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+
+        // Write permissions on sdcard?
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_STORAGE_PERMISSION_REQUEST_CODE);
+        } else {
+            startService();
+        }
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // Set JavascriptInterface
+        JSInterface = new JavaScriptInterface(this);
+        mWebView.addJavascriptInterface(JSInterface, "android");
+        mWebView.setWebContentsDebuggingEnabled(true);
+        // Load the GUI
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                // Show custom error page
+                if (view != null){
+                    String htmlData ="<html><body><div align=\"center\" >Please wait while loading backend..</div></body>";
+                    view.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8", null);
+                }
+
+                // Reload after 1s. Should happen only when IPv8 is still loading.
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWebView.loadUrl(url);
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                Log.d("openwalletWebview", "requesting: " + url);
+                return super.shouldInterceptRequest(view, url);
+            }
+        });
+        if (savedInstanceState == null) {
+            mWebView.loadUrl(url);
+        }
+    }
+
+    // </IPV8APP>
+
 
     public String getAppRoot() {
         String app_root =  getFilesDir().getAbsolutePath() + "/app";
@@ -151,6 +233,10 @@ public class PythonActivity extends Activity {
                 });
            dlgAlert.setCancelable(false);
            dlgAlert.create().show();
+       
+            // <IPV8APP>   
+            ipv8AppOnCreate(savedInstanceState);
+            // </IPV8APP>   
 
            return;
         }
